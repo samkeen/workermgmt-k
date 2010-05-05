255deb405b00988f933cbc0741f9075c31d0c29d
Sam Keen <sam@pageindigo.com>
2010-05-05 15:17:30 -0700

    adding a notify_buddy method to send email

--- modules/bugzilla/models/hiring.php
+++ modules/bugzilla/models/hiring.php
@@ -1,9 +1,6 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 class Hiring_Model {
-
-  
-
   /**
    * Class constructor.
    *
@@ -22,6 +19,29 @@ class Hiring_Model {
   }
   public function employee_attributes($ldap_email) {
       return $this->ldap->employee_attributes($ldap_email);
+  }
+  /**
+   *
+   * Available $template vars are:
+   *  {{buddy name}}
+   *  {{newhire_name}}
+   *  {{newhire_email}}
+   *  {{hiring_manager_name}}
+   *
+   *
+   */
+  public function notify_buddy($input) {
+    print_r($input);die;
+    if(SEND_EMAIL) {
+      Kohana::config_load('workermgmt');
+      $from = Kohana::config('workermgmt.email_from_address');
+      $template = Kohana::config('workermgmt.buddy_email_template');
+//      $to = isset($input['']) ? :;
+//      $mail_result = mail(implode(", ", $notified_people), $subject, $body, "From: ". $from);
+      Kohana::log('notice', $message);
+    } else {
+      Kohana::log('debug', "SEND_EMAIL is false so Buddy notification email not sent");
+    }
   }
 
   
